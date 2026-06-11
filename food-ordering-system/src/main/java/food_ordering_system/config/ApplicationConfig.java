b4cfbf25e49a720f1757b26b1d1117266724bd8c
Teboho-Rea <reabetsweteboho05@gmail.com>
2026-06-11 15:07:11 +0200

    Created application configuration class

---
+++ food-ordering-system/src/main/java/food_ordering_system/config/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package food_ordering_system.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// Configuration classes are used to define custom Spring application settings.
+@Configuration
+public class ApplicationConfig {
+}
