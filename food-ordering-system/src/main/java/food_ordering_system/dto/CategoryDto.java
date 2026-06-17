b63a0c28d62ea4e4d0a5ad68aea31116b8e9976c
Teboho-Rea <reabetsweteboho05@gmail.com>
2026-06-17 14:18:34 +0200

    feat(category): input validation

--- food-ordering-system/src/main/java/food_ordering_system/dto/CategoryDto.java
+++ food-ordering-system/src/main/java/food_ordering_system/dto/CategoryDto.java
@@ -1,5 +1,7 @@
 package food_ordering_system.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 // DTO means Data Transfer Object.
@@ -8,5 +10,8 @@ import lombok.Data;
 public class CategoryDto {
 
     private Long id;
+
+    @NotBlank(message = "Category name is required")
+    @Size(min = 2, max = 50, message = "Name must be 2-50 characters")
     private String name;
 }
